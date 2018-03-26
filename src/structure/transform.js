6fd328ef360226fb19fdbab97fafe2a968066f17
ivana <ivanafustar@hotmail.com>
2018-03-26 14:50:19 +0200

    transformation task

--- src/structure/transform.js
+++ src/structure/transform.js
@@ -23,3 +23,49 @@
 
  */
 
+
+const data = require("./data").data;
+
+function getAvailableTags(data) {
+
+    let tags = [];
+    data.forEach(value => {
+        value.tags.forEach(tag => {
+            if (!(tags.includes(tag)))
+                tags.push(tag);
+        });
+    });
+    //console.log(tags.length);
+    return tags;
+
+}
+
+//console.log(data);
+//console.log(getAvailableTags(data));
+
+function getActiveUsers(data) {
+    return data.filter(value => value.isActive);
+}
+
+
+//console.log(getActiveUsers(data));
+
+function trimUsers(data, selection) {
+    const trimmed = [];
+
+    data.forEach(object => {
+
+        let user = {};
+
+        selection.forEach(selected => {
+            user[selected] = object[selected];
+        });
+
+        trimmed.push(user);
+    });
+
+    return trimmed;
+}
+
+console.log(trimUsers(data, ['guid', 'name', 'company']));
+/**/
