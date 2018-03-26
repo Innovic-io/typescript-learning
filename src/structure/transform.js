6a566a739e437e1f70e4d832580fe869ac5ce998
Vlado Tešanović <vlado.tesanovic@gmail.com>
2018-03-26 08:51:00 +0200

    Task #2

---
+++ src/structure/transform.js
@@ -0,0 +1,25 @@
+// import data from data.js
+// make functions which collect all available tags
+// make function which return only active users
+// make function which returns trimmed response, for given input
+
+// trimUsers(['guid','name', 'gender')
+//
+// return array of users with only above fields:
+/*
+
+[
+  {
+    "guid": "c71a9c74-679f-4165-8a85-8f7020a9194c",
+    "name": "Letha Fletcher",
+    "gender": "female",
+   },
+   {
+    "guid": "c71a9c74-679f-4165-8a85-8f7020a9194c",
+    "name": "Letha Fletcher",
+    "gender": "female",
+   }
+]
+
+ */
+
