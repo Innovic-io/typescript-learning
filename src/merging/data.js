368150bdb6357346bf85ed9dbe8cdfe5eb67774d
Vlado Tešanović <vlado.tesanovic@gmail.com>
2018-03-26 15:10:37 +0200

    Task #

---
+++ src/merging/data.js
@@ -0,0 +1,512 @@
+const data = [
+    {
+        "_id": "5ab896bd31b05b1afeb22666",
+        "index": 0,
+        "guid": "c71a9c74-679f-4165-8a85-8f7020a9194c",
+        "isActive": true,
+        "balance": "$2,279.61",
+        "picture": "http://placehold.it/32x32",
+        "age": 39,
+        "married": true,
+        "eyeColor": "brown",
+        "name": "Letha Fletcher",
+        "gender": "female",
+        "company": "CUIZINE",
+        "email": "lethafletcher@cuizine.com",
+        "phone": "+1 (989) 508-2269",
+        "address": "427 Downing Street, Darbydale, Alabama, 5181",
+        "about": "Voluptate velit dolore commodo anim duis aliqua voluptate nulla enim aliquip enim reprehenderit exercitation eiusmod. Eiusmod eiusmod deserunt irure ipsum ipsum amet non anim elit nulla est. Anim dolore mollit deserunt proident non dolor culpa aliqua ex voluptate eiusmod in Lorem. Consequat minim duis officia dolore consectetur non qui eu.\r\n",
+        "registered": "2014-08-04T06:20:45 -02:00",
+        "latitude": 43.514142,
+        "longitude": 44.997897,
+        "tags": [
+            "quis",
+            "anim",
+            "velit",
+            "exercitation",
+            "veniam",
+            "do",
+            "minim"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Pate Barber"
+            },
+            {
+                "id": 1,
+                "name": "Mcpherson Delgado"
+            },
+            {
+                "id": 2,
+                "name": "Osborn Herrera"
+            }
+        ],
+        "greeting": "Hello, Letha Fletcher! You have 2 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "5ab896bdd75a30edbaf877a5",
+        "index": 1,
+        "guid": "2074cd54-aaac-4e81-a083-7de24ff7cc02",
+        "isActive": false,
+        "balance": "$1,348.42",
+        "picture": "http://placehold.it/32x32",
+        "age": 25,
+        "married": false,
+        "eyeColor": "brown",
+        "name": "Reva Calhoun",
+        "gender": "female",
+        "company": "CENTREXIN",
+        "email": "revacalhoun@centrexin.com",
+        "phone": "+1 (896) 453-3385",
+        "address": "147 Delevan Street, Starks, Louisiana, 9838",
+        "about": "Sit sint adipisicing veniam aute do dolor. Culpa pariatur ea mollit aliquip dolor sint amet. Adipisicing velit non cupidatat officia magna qui eu adipisicing qui Lorem tempor. Dolor occaecat eu ex incididunt non nisi amet sint aliqua. Magna minim voluptate excepteur tempor occaecat reprehenderit dolore incididunt non est dolor. Mollit culpa magna dolore ea laborum laboris id irure fugiat dolore. Sint ut labore deserunt dolor culpa nostrud sit culpa fugiat.\r\n",
+        "registered": "2015-10-09T07:51:00 -02:00",
+        "latitude": 2.029733,
+        "longitude": 25.499677,
+        "tags": [
+            "reprehenderit",
+            "nostrud",
+            "enim",
+            "in",
+            "occaecat",
+            "nulla",
+            "nostrud"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Jimenez Ayers"
+            },
+            {
+                "id": 1,
+                "name": "Puckett Moran"
+            }
+        ],
+        "greeting": "Hello, Reva Calhoun! You have 9 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "5ab896bdec3269e0c0ca70c8",
+        "index": 2,
+        "guid": "4b73b45f-fc4a-4ac5-8b1e-4d2cbaf7291e",
+        "isActive": false,
+        "balance": "$1,060.14",
+        "picture": "http://placehold.it/32x32",
+        "age": 33,
+        "married": true,
+        "eyeColor": "brown",
+        "name": "Barber Huff",
+        "gender": "male",
+        "company": "NSPIRE",
+        "email": "barberhuff@nspire.com",
+        "phone": "+1 (859) 525-2797",
+        "address": "491 Dunne Court, Holtville, Marshall Islands, 5385",
+        "about": "Minim veniam sunt sunt tempor Lorem anim aute cillum ullamco occaecat non in proident. Sunt commodo mollit reprehenderit duis pariatur esse veniam nulla amet cillum qui eiusmod magna. Eiusmod ea nostrud pariatur nostrud irure enim culpa. Ad eu labore laboris laborum excepteur anim. Exercitation nostrud sunt occaecat cupidatat sint anim minim aliquip nulla consequat incididunt amet ipsum magna.\r\n",
+        "registered": "2015-06-08T06:45:51 -02:00",
+        "latitude": 15.338671,
+        "longitude": 65.399784,
+        "tags": [
+            "minim",
+            "ea",
+            "consequat",
+            "non",
+            "ea",
+            "non",
+            "dolor"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Jacklyn Gillespie"
+            },
+            {
+                "id": 1,
+                "name": "Velma Sloan"
+            },
+            {
+                "id": 2,
+                "name": "Rosalyn William"
+            },
+            {
+                "id": 3,
+                "name": "Velma Sloan"
+            },
+        ],
+        "greeting": "Hello, Barber Huff! You have 3 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "5ab896bd4afe865214365fdf",
+        "index": 3,
+        "guid": "dceed7ff-82fd-49ce-ae3e-bccb1548e556",
+        "isActive": false,
+        "balance": "$2,982.12",
+        "picture": "http://placehold.it/32x32",
+        "age": 37,
+        "married": true,
+        "eyeColor": "green",
+        "name": "Harriet Spears",
+        "gender": "female",
+        "company": "XYMONK",
+        "email": "harrietspears@xymonk.com",
+        "phone": "+1 (879) 590-3522",
+        "address": "419 Krier Place, Fannett, Oklahoma, 4348",
+        "about": "Enim culpa magna nostrud cupidatat commodo laborum pariatur duis voluptate esse minim laborum. Magna excepteur commodo magna sint duis ad adipisicing. Est officia dolore laboris culpa pariatur eu exercitation mollit voluptate aliquip. Excepteur Lorem occaecat ullamco qui enim magna commodo eiusmod. Ullamco nulla et aute incididunt sunt consequat.\r\n",
+        "registered": "2018-02-19T11:30:36 -01:00",
+        "latitude": 76.115833,
+        "longitude": 15.876087,
+        "tags": [
+            "pariatur",
+            "deserunt",
+            "aute",
+            "enim",
+            "cupidatat",
+            "aute",
+            "veniam"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Shaffer Ortiz"
+            },
+            {
+                "id": 1,
+                "name": "Marjorie Wade"
+            },
+            {
+                "id": 2,
+                "name": "Alisha Long"
+            }
+        ],
+        "greeting": "Hello, Harriet Spears! You have 9 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "5ab896bd15479f47b4458524",
+        "index": 4,
+        "guid": "005c3938-d76e-4710-a990-b1b01fc32003",
+        "isActive": true,
+        "balance": "$1,773.98",
+        "picture": "http://placehold.it/32x32",
+        "age": 22,
+        "married": false,
+        "eyeColor": "green",
+        "name": "Claudia Orr",
+        "gender": "female",
+        "company": "QUAILCOM",
+        "email": "claudiaorr@quailcom.com",
+        "phone": "+1 (866) 414-3045",
+        "address": "783 Scholes Street, Woodlake, Wyoming, 682",
+        "about": "Irure dolore aliqua et anim laboris ullamco quis eiusmod nisi sint irure sit nulla enim. Ipsum proident non fugiat ullamco. Nulla cillum tempor dolor irure cupidatat enim elit aute dolore ut. Sunt pariatur exercitation do incididunt elit cupidatat id veniam ipsum. Ex ex ullamco occaecat consectetur deserunt laborum quis cupidatat ullamco labore laboris.\r\n",
+        "registered": "2015-09-22T09:12:50 -02:00",
+        "latitude": 75.005054,
+        "longitude": -151.405665,
+        "tags": [
+            "labore",
+            "occaecat",
+            "eiusmod",
+            "officia",
+            "sint",
+            "in",
+            "laboris"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Keller Cook"
+            },
+            {
+                "id": 1,
+                "name": "Glass Conway"
+            },
+            {
+                "id": 2,
+                "name": "Smith Barton"
+            }
+        ],
+        "greeting": "Hello, Claudia Orr! You have 8 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "5ab896bd45109ee287752292",
+        "index": 5,
+        "guid": "da245db1-e34a-43d4-851b-f9ccb07edb20",
+        "isActive": true,
+        "balance": "$3,297.76",
+        "picture": "http://placehold.it/32x32",
+        "age": 31,
+        "married": true,
+        "eyeColor": "green",
+        "name": "Susanne May",
+        "gender": "female",
+        "company": "QUAREX",
+        "email": "susannemay@quarex.com",
+        "phone": "+1 (935) 533-2520",
+        "address": "316 Macon Street, Bowie, Nebraska, 6206",
+        "about": "Labore tempor aute ipsum irure sunt consectetur do ullamco esse enim tempor dolor. Dolor est in consectetur nostrud labore laboris proident non deserunt consequat ut voluptate. Aliquip Lorem veniam velit duis incididunt id id magna nisi amet proident id deserunt.\r\n",
+        "registered": "2015-04-28T02:42:36 -02:00",
+        "latitude": 18.5474,
+        "longitude": 82.379535,
+        "tags": [
+            "sint",
+            "et",
+            "est",
+            "consequat",
+            "laborum",
+            "laborum",
+            "deserunt"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Petty Murray"
+            },
+            {
+                "id": 1,
+                "name": "Marietta Steele"
+            },
+            {
+                "id": 2,
+                "name": "Zimmerman Taylor"
+            }
+        ],
+        "greeting": "Hello, Susanne May! You have 5 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "5ab896bd2818ae09c5b22bc0",
+        "index": 6,
+        "guid": "53586a36-25f7-47d7-9365-2385b5bce5b8",
+        "isActive": true,
+        "balance": "$3,782.40",
+        "picture": "http://placehold.it/32x32",
+        "age": 26,
+        "married": false,
+        "eyeColor": "blue",
+        "name": "Reyna Vincent",
+        "gender": "female",
+        "company": "BIZMATIC",
+        "email": "reynavincent@bizmatic.com",
+        "phone": "+1 (949) 487-2207",
+        "address": "391 Atlantic Avenue, Edmund, Idaho, 5151",
+        "about": "Magna anim cillum quis laborum eiusmod quis occaecat cupidatat id adipisicing aliqua anim fugiat. Enim non Lorem sit tempor. Quis et ullamco officia reprehenderit velit laboris duis fugiat aute.\r\n",
+        "registered": "2015-05-25T08:46:38 -02:00",
+        "latitude": -44.518661,
+        "longitude": -136.619957,
+        "tags": [
+            "consequat",
+            "tempor",
+            "tempor",
+            "adipisicing",
+            "ex",
+            "deserunt",
+            "nulla"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Meyer Talley"
+            },
+            {
+                "id": 1,
+                "name": "Wilkinson Goodman"
+            },
+            {
+                "id": 2,
+                "name": "Armstrong Fuller"
+            }
+        ],
+        "greeting": "Hello, Reyna Vincent! You have 4 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "5ab896bd2489b0355c8f9271",
+        "index": 7,
+        "guid": "f7540ba5-62f4-4dbb-a420-647be3409b58",
+        "isActive": true,
+        "balance": "$2,615.02",
+        "picture": "http://placehold.it/32x32",
+        "age": 27,
+        "married": false,
+        "eyeColor": "green",
+        "name": "Castillo Huber",
+        "gender": "male",
+        "company": "INTERLOO",
+        "email": "castillohuber@interloo.com",
+        "phone": "+1 (888) 517-3719",
+        "address": "668 Lake Avenue, Matthews, Alaska, 3207",
+        "about": "Eiusmod fugiat dolor aute est. Aliquip officia labore id duis et nulla ullamco labore ipsum. Ipsum excepteur veniam deserunt ex sit velit dolore Lorem exercitation velit. Tempor sint sint voluptate nulla commodo magna occaecat proident. Ullamco ex irure sint duis et fugiat pariatur velit duis. Cillum ex occaecat commodo aute ullamco deserunt sit sint amet Lorem.\r\n",
+        "registered": "2014-08-18T11:25:27 -02:00",
+        "latitude": -10.829641,
+        "longitude": 74.548865,
+        "tags": [
+            "adipisicing",
+            "occaecat",
+            "culpa",
+            "eu",
+            "do",
+            "officia",
+            "ullamco"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Webster Short"
+            },
+            {
+                "id": 1,
+                "name": "Espinoza Frederick"
+            },
+            {
+                "id": 2,
+                "name": "Betty Cox"
+            }
+        ],
+        "greeting": "Hello, Castillo Huber! You have 9 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "5ab896bd73f300280a3f5410",
+        "index": 8,
+        "guid": "a0ced07b-9d4d-4f24-ae8e-8e279770cf18",
+        "isActive": true,
+        "balance": "$3,744.92",
+        "picture": "http://placehold.it/32x32",
+        "age": 40,
+        "married": true,
+        "eyeColor": "brown",
+        "name": "Carole Santana",
+        "gender": "female",
+        "company": "AQUACINE",
+        "email": "carolesantana@aquacine.com",
+        "phone": "+1 (859) 417-3610",
+        "address": "992 Harrison Place, Brooktrails, North Carolina, 2268",
+        "about": "Consequat consectetur ullamco consectetur sint dolore sint aliquip officia ullamco. Labore dolore in sunt tempor tempor ullamco pariatur veniam cupidatat laborum occaecat laboris irure. Incididunt nostrud sit ea velit.\r\n",
+        "registered": "2017-01-01T11:38:53 -01:00",
+        "latitude": -24.859838,
+        "longitude": -127.612575,
+        "tags": [
+            "quis",
+            "id",
+            "excepteur",
+            "pariatur",
+            "ea",
+            "pariatur",
+            "voluptate"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Trisha Cain"
+            },
+            {
+                "id": 1,
+                "name": "Boyd Johnston"
+            },
+            {
+                "id": 2,
+                "name": "Morse Simpson"
+            }
+        ],
+        "greeting": "Hello, Carole Santana! You have 6 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "5ab896bd229d1201734c7baf",
+        "index": 9,
+        "guid": "69c8d856-fe08-4e3d-a338-8fddb8820dc6",
+        "isActive": false,
+        "balance": "$3,728.69",
+        "picture": "http://placehold.it/32x32",
+        "age": 37,
+        "married": true,
+        "eyeColor": "green",
+        "name": "Sandy Lowe",
+        "gender": "female",
+        "company": "FIREWAX",
+        "email": "sandylowe@firewax.com",
+        "phone": "+1 (822) 560-2077",
+        "address": "803 Cranberry Street, Aguila, Georgia, 9598",
+        "about": "Nulla nisi non labore deserunt proident occaecat tempor commodo. Exercitation veniam excepteur velit nulla adipisicing adipisicing officia dolore laborum amet. Incididunt ad aliquip aute sint. Esse nulla ipsum in sunt laboris voluptate nulla aute mollit veniam exercitation adipisicing velit. Pariatur fugiat magna amet nisi duis laborum do officia nulla sit veniam.\r\n",
+        "registered": "2015-10-17T08:28:55 -02:00",
+        "latitude": -45.965075,
+        "longitude": -0.223727,
+        "tags": [
+            "amet",
+            "exercitation",
+            "et",
+            "sit",
+            "ad",
+            "consequat",
+            "fugiat"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Conrad Lindsey"
+            },
+            {
+                "id": 1,
+                "name": "Francis Wagner"
+            },
+            {
+                "id": 2,
+                "name": "Franks Cash"
+            }
+        ],
+        "greeting": "Hello, Sandy Lowe! You have 7 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "5ab896bd0cc9010d95ba614a",
+        "index": 10,
+        "guid": "88f3b849-8817-4c78-b186-46286a130628",
+        "isActive": true,
+        "balance": "$2,809.80",
+        "picture": "http://placehold.it/32x32",
+        "age": 37,
+        "married": true,
+        "eyeColor": "blue",
+        "name": "Beasley Klein",
+        "gender": "male",
+        "company": "COMSTRUCT",
+        "email": "beasleyklein@comstruct.com",
+        "phone": "+1 (846) 582-2423",
+        "address": "208 Calyer Street, Navarre, New Hampshire, 9586",
+        "about": "Reprehenderit sint veniam adipisicing velit cillum laborum ipsum consequat ut non minim. Ad ex tempor eiusmod velit minim sint sint velit quis quis cupidatat aliqua sit officia. Enim consequat nisi magna est ullamco ea enim eu quis. Ut do exercitation eiusmod exercitation magna adipisicing.\r\n",
+        "registered": "2015-10-28T12:10:27 -01:00",
+        "latitude": 76.218027,
+        "longitude": 139.520297,
+        "tags": [
+            "tempor",
+            "voluptate",
+            "consequat",
+            "non",
+            "nisi",
+            "fugiat",
+            "esse"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Townsend Cunningham"
+            },
+            {
+                "id": 1,
+                "name": "Amber Bender"
+            },
+            {
+                "id": 2,
+                "name": "Neal Weaver"
+            }
+        ],
+        "greeting": "Hello, Beasley Klein! You have 5 unread messages.",
+        "favoriteFruit": "strawberry"
+    }
+];
+
+module.exports = {
+    data
+};
---
+++ src/merging/merging.js
@@ -0,0 +1,13 @@
+// create function which:
+
+// 1. remove duplicate tags from user object.
+// 2. calculate how many friends user have
+// 3. create array of strings where each string is sentence that describe user:
+
+/*
+const ouput = [
+'Mrs. Letha Fletcher from 427 Downing Street, Darbydale, Alabama, 5181',
+'Mr. Leva Calhoun from 147 Delevan Street, Starks, Louisiana, 98385181'
+]
+
+ */
